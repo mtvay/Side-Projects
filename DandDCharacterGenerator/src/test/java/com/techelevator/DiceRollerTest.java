f0eb4a78575ea4ef79255c0dd70058577d006db9
Mike Vay <mikevay@gmail.com>
2021-01-28 12:26:34 -0500

    created DiceRollerTest and renamed description to Describable for proper interface naming

---
+++ DandDCharacterGenerator/src/test/java/com/techelevator/DiceRollerTest.java
@@ -0,0 +1,32 @@
+package com.techelevator;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class DiceRollerTest {
+	
+	@Test
+	public void rollDice_does_not_return_zero_or_number_higher_than_sides_of_dice() {
+		//Arrange
+		
+		
+		//Act
+		
+		
+		//Assert
+		
+	}
+	
+	@Test
+	public void abilityScoreRoll_does_not_include_zero_or_return_over_18() {
+		//Arrange
+		
+		
+		//Act
+		
+		
+		//Assert
+		
+	}
+
+}
